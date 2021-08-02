56711aa0144677c00301597038c8f6010112d92d
Vipul Jha <vipuljha08@gmail.com>
2021-08-02 16:19:17 +0530

    Set base for Clean Architecture
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:moviez/screens/home_page.dart';
+import 'package:moviez/presentation/journeys/home_page.dart';
 
 void main() {
   runApp(Moviez());
