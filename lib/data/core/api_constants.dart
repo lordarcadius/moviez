42525a045def228df226a9cc35e91ec9e07e6035
Vipul Jha <vipuljha08@gmail.com>
2021-08-02 16:40:12 +0530

    Remove cache
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- lib/data/core/api_constants.dart
+++
@@ -1,7 +0,0 @@
-class ApiConstants {
-  ApiConstants._();
-
-  static const String BASE_URL = "https://api.themoviedb.org/4/";
-  static const String API_KEY = "PUT YOUR API KEY HERE!";
-  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
-}
