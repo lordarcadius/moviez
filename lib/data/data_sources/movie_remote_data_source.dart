157faab671d2ab53556df39467986f1fee51a0a9
Vipul Jha <vipuljha08@gmail.com>
2021-08-03 15:24:37 +0530

    data: Move data parse logic to ApiClient
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- lib/data/data_sources/movie_remote_data_source.dart
+++ lib/data/data_sources/movie_remote_data_source.dart
@@ -1,33 +1,29 @@
-import 'dart:convert';
-
-import 'package:http/http.dart';
+import 'package:moviez/data/core/api_client.dart';
 import 'package:moviez/data/models/movie_model.dart';
 import 'package:moviez/data/models/movies_result_model.dart';
-import '../core/api_constants.dart';
 
 abstract class MovieRemoteDataSource {
   Future<List<MovieModel>> getTrending();
+  Future<List<MovieModel>> getPopular();
 }
 
 class MovieRemoteDataSourceImpl extends MovieRemoteDataSource {
-  final Client _client;
+  final ApiClient _client;
 
   MovieRemoteDataSourceImpl(this._client);
   @override
   Future<List<MovieModel>> getTrending() async {
-    final response = await _client.get(
-      Uri.parse(
-          '${ApiConstants.BASE_URL}trending/movie/day?api_key=${ApiConstants.API_KEY}'),
-      headers: {'Content-Type': 'application/json'},
-    );
+    final response = await _client.get('trending/movie/day');
+    final movies = MoviesResultModel.fromJson(response).movies;
+    print(movies);
+    return movies;
+  }
 
-    if (response.statusCode == 200) {
-      final responseBody = json.decode(response.body);
-      final movies = MoviesResultModel.fromJson(responseBody).movies;
-      print(movies);
-      return movies;
-    } else {
-      throw new Exception(response.reasonPhrase);
-    }
+  @override
+  Future<List<MovieModel>> getPopular() async {
+    final response = await _client.get('movie/popular');
+    final movies = MoviesResultModel.fromJson(response).movies;
+    print(movies);
+    return movies;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:moviez/data/core/api_client.dart';
 import 'package:moviez/data/data_sources/movie_remote_data_source.dart';
 import 'package:moviez/presentation/journeys/home_page.dart';
 
 void main() {
-  Client apiClient = new Client();
+  ApiClient apiClient = ApiClient(Client());
   MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
   dataSource.getTrending();
+  dataSource.getPopular();
 
   runApp(Moviez());
 }
